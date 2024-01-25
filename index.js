18afc5f0e20489d0ad0cbb2434ada3e5af6a67a7
DianaTuei <memotuei@gmail.com>
2024-01-25 10:56:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // input - word
+  // output - true or false
+
+  // first convert word to lowercase
+  let smallLettersWord = word.toLowerCase()
+
+  // remove non alphabetic characters
+  smallLettersWord = smallLettersWord.replace(/[^a-z]/g, '')
+
+  // compare lower case word with its reverse
+  if( smallLettersWord === smallLettersWord.split('').reverse().join('')){
+    return true
+  } else {
+    return false
+  }
+  
 }
+
+isPalindrome('abba')
+console.log(isPalindrome('abba'))
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('a'))
+console.log(isPalindrome('robot'))
+console.log(isPalindrome('ab'))
 
 /* 
   Add your pseudocode here
 */
+// pseudocode
+// declare a variable
+// make the variable split
+// then iterate through each word to ensure the letters are the same, that is first and last, second and second last
 
+// then return this function with a word
 /*
   Add written explanation of your solution here
 */
